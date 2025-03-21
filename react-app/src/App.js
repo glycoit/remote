7784eda3266a0c454ec67a465166a9d1f1edb285
Nizar Ismail <nizar.ismail@inceptua.com>
2025-03-21 02:38:40 +0100

    Initial commit of PharmaDashboard app

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PharmaDashboard from "./PharmaDashboard";  // Import your PharmaDashboard component
+
+function App() {
+  return (
+    <div className="App">
+      {/* Use the PharmaDashboard component */}
+      <PharmaDashboard />  
+    </div>
+  );
+}
+
+export default App;
