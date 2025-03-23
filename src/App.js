82760be30d47346ffe5951d2e95c4755c930389f
Nizar Ismail <nizar.ismail@inceptua.com>
2025-03-23 16:14:40 +0100

    Deploy to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PharmaDashboard from "./pharmadashboard";  // Ensure the filename matches exactly
+
+function App() {
+  return (
+    <div className="App">
+      {/* Use the PharmaDashboard component */}
+      <PharmaDashboard />  
+    </div>
+  );
+}
+
+export default App;
