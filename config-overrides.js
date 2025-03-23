c968047d873157aff17340d3dedaec6514de6674
Nizar Ismail <nizar.ismail@inceptua.com>
2025-03-23 21:07:21 +0100

    Updates

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = function override(config, env) {
-  config.resolve.fallback = {
-    ...config.resolve.fallback,
-    os: require.resolve('os-browserify/browser'),
-  };
-  return config;
-};
